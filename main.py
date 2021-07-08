8df260603836362595b5794e360934887317b4e6
Andrii <AndriiUA@outlook.com>
2021-07-08 06:17:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+aws = pd.read_csv('acme_worksheet.csv')
+aws.Date = (pd.to_datetime(aws.Date, format='%b %d %Y'))
+
+new_aws = pd.pivot_table(aws, index='Employee Name', columns='Date', values='Work Hours')
+new_aws.index.name = 'Name / Date'
+new_aws.columns = new_aws.columns.strftime('%Y-%m-%d')
+new_aws.fillna(0, inplace=True)
+print(new_aws)
+new_aws.to_csv('new_acme_worksheet.csv')
